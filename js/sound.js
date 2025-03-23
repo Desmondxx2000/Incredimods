374c1e58e00ec888e39792b3f81e4b7d516d41f2
Desmondxx2000 <desmondbrown20000@gmail.com>
2025-03-23 19:25:04 -0400

    Update sound.js

--- js/sound.js
+++ js/sound.js
@@ -1,9 +1,13 @@
+var delayInMilliseconds = 6857;
 var downtownAudio = new Audio('../assets/audio/mp3/flakey-flake.mp3');
-     var bAudio = new Audio('b.mp3');
-     function myAudioFunction(soundCue) {
-         if(soundCue == 'downtown') {
-             aAudio.play();
-         } else if(letter == 'b') {
-             bAudio.play();
-         }
+var bAudio = new Audio('b.mp3');
+
+setTimeout(function() {
+function myAudioFunction(soundCue) {
+     if(soundCue == 'downtown') {
+          aAudio.play();
+     } else if(letter == 'b') {
+          bAudio.play();
      }
+}
+}, delayInMilliseconds);
